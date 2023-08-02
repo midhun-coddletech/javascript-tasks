ba7f36a6b01e20d421c94dc85f05d40a10963b6e
midhun-coddletech <midhun.o@coddletech.com>
2023-08-02 13:16:26 +0530

    fixed the bugs in the given assignment

--- assignment7/js_troubleshooting/js_troubleshooting/js/slider.js
+++ assignment7/js_troubleshooting/js_troubleshooting/js/slider.js
@@ -1,16 +1,16 @@
-let a = ["1.jpg", "2.jpg", "3.jpg"];
-const selectedIndex = 0;
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const a = ['1.jpg', '2.jpg', '3.jpg'];
+let selectedIndex = 0;
 
 function updateImg(type) {
-  if (type === 0) {
-    selectedIndex--;
-  } else selectedIndex++;
-
-  if (selectedIndex <= 0) {
-    selectedIndex = a.length - 1;
-  } else if (selectedIndex >= a.length) {
-    selectedIndex = 0;
-  }
-
-  document.getElementsById("img").src = "./images" + a[selectedIndex];
+    if (type === '0') {
+        selectedIndex -= 1;
+    } else selectedIndex += 1;
+    if (selectedIndex < 0) {
+        selectedIndex = a.length - 1;
+    } else if (selectedIndex >= a.length) {
+        selectedIndex = 0;
+    }
+    document.getElementById('imgs').src = `./images/${a[selectedIndex]}`;
 }
